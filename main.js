2fdf62d045bf837a5db02d551465c32ea3ebe5a3
Satizh <33228593+Sa-tizh@users.noreply.github.com>
2020-01-15 22:14:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-var tableHeader2 = document.getElementById("th2");
-var tableHeader3 = document.getElementById("th3");
-var tableHeader4 = document.getElementById("th4");
-
 var n = 0;
 
-var tableHeader1 = document.getElementById("th1");
-tableHeader1.addEventListener("click", function() {n = 0; sortTable(n);});
-tableHeader2.addEventListener("click", function() {n = 1; sortTable(n);});
-tableHeader3.addEventListener("click", function() {n = 2; sortTable(n);});
-tableHeader4.addEventListener("click", function() {n = 3; sortTable(n);});
+document.getElementById("th1").addEventListener("click", function() {n = 0; sortTable(n);});
+document.getElementById("th2").addEventListener("click", function() {n = 1; sortTable(n);});
+document.getElementById("th3").addEventListener("click", function() {n = 2; sortTable(n);});
+document.getElementById("th4").addEventListener("click", function() {n = 3; sortTable(n);});
 
 function sortTable(n) { 
   var table, i, x, y; 
