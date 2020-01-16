d4e90099825fe1fb17bff923622e48ee60b081f2
Satizh <33228593+Sa-tizh@users.noreply.github.com>
2020-01-16 16:26:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -59,10 +59,10 @@ window.onload = function(){
     window.setInterval(updateTable, 2000)
   
     function updateTable(){
-      var currTable = document.getElementById("table1");
+      var currTable = document.getElementById("table1").childNodes[0];
       var tableLength = currTable.childNodes.length;
       
-      for(var i = 2; i < tableLength - 2; i++){
+      for(var i = 1; i < tableLength - 2; i++){
          currTable.removeChild(currTable.childNodes[i])
       }
     }
@@ -101,7 +101,7 @@ window.onload = function(){
   
             //this function appends the json data to the table 'gable'
             function append_json(data){
-                var table = document.getElementById("table1");
+                var table = document.getElementById("table1").childNodes[0];
                 console.log(JSON.stringify(data));
                 data.forEach(function(object) {
                     var tr = document.createElement('tr');
