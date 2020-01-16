5c9afc01ed0ae23f8ebcffd5e70d9c7ec0a933fa
Satizh <33228593+Sa-tizh@users.noreply.github.com>
2020-01-16 16:48:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -67,18 +67,7 @@ window.onload = function(){
       }
     }
     // __________________________________________
-  
-  }
-  
-  
-  
-  //first add an event listener for page load
-            document.addEventListener( "DOMContentLoaded", get_json_data, false ); // get_json_data is the function name that will fire on page load
-  
-            //this function is in the event listener and will execute on page load
-            function get_json_data(){
-  
-                // Relative URL of external json file
+    // Relative URL of external json file
                 var json_url = "https://wt.ops.labs.vu.nl/api20/14d61d4c/";
   
                 //Build the XMLHttpRequest (aka AJAX Request)
@@ -113,4 +102,49 @@ window.onload = function(){
                     
                     $(tr).insertBefore(table.lastChild);
                 });
+  }
+  
+  
+  
+  //first add an event listener for page load
+            document.addEventListener( "DOMContentLoaded", get_json_data, false ); // get_json_data is the function name that will fire on page load
+  
+            //this function is in the event listener and will execute on page load
+            function get_json_data(){
+  
+                /*// Relative URL of external json file
+                var json_url = "https://wt.ops.labs.vu.nl/api20/14d61d4c/";
+  
+                //Build the XMLHttpRequest (aka AJAX Request)
+                xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() { 
+                    console.log(this.readyState + " " + this.status);
+                    if (this.readyState == 4 && this.status == 200) {//when a good response is given do this
+  
+                        var data = JSON.parse(this.responseText); // convert the response to a json object
+                        append_json(data);// pass the json object to the append_json function
+                    }
+                }
+                //set the request destination and type
+                xmlhttp.open("GET", json_url, true);
+                //set required headers for the request
+                xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                // send the request
+                xmlhttp.send(); // when the request completes it will execute the code in onreadystatechange section
+            }
+  
+            //this function appends the json data to the table 'gable'
+            function append_json(data){
+                var table = document.getElementById("tablebody1");
+                console.log(JSON.stringify(data));
+                data.forEach(function(object) {
+                    var tr = document.createElement('tr');
+                    tr.innerHTML = '<td>' + object.brand + '</td>' +
+                                    '<td>' + object.model + '</td>' +
+                                    '<td>' + object.os + '</td>' +
+                                    '<td>' + object.screensize + '</td>' +
+                                    '<td> <img class="defaultImg" src="' +  object.image + '"></td>';
+                    
+                    $(tr).insertBefore(table.lastChild);
+                });*/
             }
