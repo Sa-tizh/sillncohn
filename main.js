e53225dc7d931fbed3f478368950e88e81056acf
Satizh <33228593+Sa-tizh@users.noreply.github.com>
2020-01-17 16:12:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ window.onload = function(){
         xhttpreset.send();
     }
   
-    window.setInterval(updateTable, 2000)
+    window.setInterval(updateTable, 200)
   
     function updateTable(){
       var currTable = document.getElementById("tablebody1");
